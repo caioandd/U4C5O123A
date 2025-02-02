c2a55b097f64c25973ed18fc373d34f94176f0b7
Caio A. da C. Andrade <caio.andrade@live.com>
2025-02-02 08:43:10 -0300

    Link do ensaio

--- README.md
+++ README.md
@@ -2,8 +2,8 @@
 
 ## Materiais necessários
 1) Microcontrolador Raspberry Pi Pico W.
-2) 03 LEDs (vermelho, amarelo e verde).
-3) 03 Resistores de 330 Ω.
+2) 02 LEDs (vermelho e verde).
+3) 02 Resistores de 330 Ω.
 
 ## Funcionalidades do programa
 
@@ -12,11 +12,9 @@ cor vermelha, alterando para amarela e, em seguida, verde.
 2) O intervalo entre os estados é de 3000ms.
 4) A rotina principal imprime uma mensagem de funcionamento a cada 1000ms.
 
-## Característocas
-1) A mudança de estado dos LEDs deve ser implementa na função
-de call-back do temporizador, a exemplo da rotina trabalhada na
-aula síncrona - repeating_timer_callback().
+## Características
+1) A mudança de estado dos LEDs foi implementa na função de call-back do temporizador, a exemplo da rotina trabalhada na aula síncrona - repeating_timer_callback().
 2) Para utilizar o sinal de cor amarela na placa BitDogLab, foi necessaria a utilização dos LEDS vermelho e verde.
 
 ## Ensaio
-<url>
+https://drive.google.com/file/d/1Mh-ybq4Ut-AJisVfRp6H4ZCRgWKspk-C/view?usp=sharing
--- semaforo.c
+++ semaforo.c
@@ -2,32 +2,33 @@
 #include "pico/stdlib.h"
 #include "hardware/timer.h"
 
+//GPIOS
 #define led_red 13
 #define led_green 11
 
-volatile int estado = 0;
+volatile int estado = 0; //Estado atual da repetição do callback
 
 void leds();
 
 bool repeating_timer_callback(struct repeating_timer *t){
     switch (estado){
       case 0:
-        printf("Sinal vermelho!\n");
+        printf("Sinal vermelho!\n"); //Acende cor vermelha
         gpio_put(led_red, true);
         gpio_put(led_green, false);
         break;
       case 1:
         printf("Sinal amarelo!\n");
-        gpio_put(led_green, true); //Vermelho + verde = amarelo
+        gpio_put(led_green, true); //Acende cor amarela (Vermelho + verde = amarelo)
         break;
       case 2:
-        printf("Sinal verde!\n");
+        printf("Sinal verde!\n"); //Acende cor verde
         gpio_put(led_red, false);
         break;
     }
-    estado++;
+    estado++; //Incremento após conclusão do case
     if (estado>2){
-      estado=0;
+      estado=0; //Limite de incremento
     }
   return true;
 }
@@ -37,15 +38,14 @@ int main(){
   leds();
 
   struct repeating_timer timer;
-  add_repeating_timer_ms(3000, repeating_timer_callback, NULL, &timer);
+  add_repeating_timer_ms(3000, repeating_timer_callback, NULL, &timer); //Chama função callback a cada 3000ms
   printf("O semáforo está inicializando...\n");
   sleep_ms(3000);
   while (true){
-    printf("O semáforo está em funcionamento.\n");
+    printf("O semáforo está em funcionamento.\n"); //Repetição principal a cada 1000ms
     sleep_ms(1000);
   }
 }
-
 //Inicialização de LEDs
 void leds(){
   gpio_init(led_red);
